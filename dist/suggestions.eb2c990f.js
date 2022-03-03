972368f7e822db58e2c57613b8524970312501ec
Hartaithan <hartaithan@gmail.com>
2022-03-03 09:51:44 +0000

    revert parcel update

--- dist/suggestions.eb2c990f.js
+++ dist/suggestions.eb2c990f.js
@@ -2770,7 +2770,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42189" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44355" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
